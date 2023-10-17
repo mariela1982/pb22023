8a51143173f3fcb9ae0090e405a1527a5c49d21f
mariela1982 <marimanri_24@hotmail.com>
2023-10-17 17:09:48 -0300

    smart2

--- Test/ar/edu/unlam/TpHerencia/TestReloj.java
+++ Test/ar/edu/unlam/TpHerencia/TestReloj.java
@@ -20,5 +20,38 @@ public class TestReloj {
 		assertNotNull(nuevoSmart);
 		
 	}
+	
+	@Test
+	public void queSePuedaContarLosPasos() {
+		String nombre = "casio";
+		String nombre1 = "carla";
+		Integer edad = 35;
+		Double peso = 78.50;
+		Double altura  = 1.60;
+		Integer modo = 1;
+		Usuario nuevo = new Usuario(nombre1, edad,peso,altura);
+		Reloj nuevoSmart = new Reloj(nombre,nuevo);
+		
+		nuevoSmart.activarPasos();
+		switch(modo) {
+		case 1 : //caminata
+			break;
+		case 2 : // carrera
+			break;
+		case 3 : // ciclismo
+			break;
+		case 4 : // natacion 
+			break;
+		}
+		
+		
+		
+		
+		
+		assertNotNull(nuevoSmart);
+		
+	}
+	
+	
 
 }
